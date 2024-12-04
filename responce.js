db0df97c411083a58b4fd889c8fd956f7b919fab
elentsygankova <lelacute2005@gmail.com>
2024-12-04 04:54:46 +0300

    Update responce.js

--- responce.js
+++ responce.js
@@ -1,95 +1,39 @@
-function sort() {
-    let price = document.getElementById("price");
-    let title = document.getElementById("title");
-    if (price.checked) {
-        document.getElementById('node_for_insert').innerHTML = '';
-        getResponce();
-    }
-    if (title.checked) {
-        document.getElementById('node_for_insert').innerHTML = '';
-        getResponce1();
-    }
+let content = [];
+
+async function getResponse() {
+    let response = await fetch("shop.json");
+    console.log("response:\n", response, "\n /response: \n");
+    let data = await response.text();
+    console.log("await response.text()\n", data);
+    content = JSON.parse(data);
+    content = content.slice(0, 9);
+    console.log("content.slice(0, 9)", content);
+    
+    renderProducts(content);
 }
 
-function search() {
-    if (price.checked) {
-        document.getElementById('node_for_insert').innerHTML = '';
-        getResponce();
-    }
-    if (title.checked) {
-        document.getElementById('node_for_insert').innerHTML = '';
-        getResponce1();
-    }
-}
-
-async function getResponce() {
-    let responce = await fetch("shop.json");
-    let content = await responce.text();
-    content = JSON.parse(content);
-    content = content.splice(0, 11);
-    let key;
-    content_price = content.sort((a, b) => a.price - b.price);
-    content_filter = [];
-    let word = document.getElementById('search').value.toLowerCase();
-    content_filter = content_price.filter((product) => {
-        return (
-            product.title.toLowerCase().includes(word) ||
-            product.description.toLowerCase().includes(word) ||
-            product.price.toString().includes(word)
-        );
-    });
+function renderProducts(products) {
     let node_for_insert = document.getElementById("node_for_insert");
-    for (key in content_filter) {
+    node_for_insert.innerHTML = '';
+    products.forEach(item => {
         node_for_insert.innerHTML += `
-        <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
-        <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
-        <h5 class="card-title">${content_filter[key].title}</h5>
-        <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
-        <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
-        <p class="card-text" >!!Заказать!! <input class="w-25" type="text" value="0" name="check"></p>
-        </li>
-                `;
-    }
+            <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 180px" class="align-self-center" src="${item.img}">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.description}. Цена ${item.price} р.</p>
+                <input type="hidden" name="vendor_code" value="${item.vendor_code}">
+                <p class="card-text">Заказать <input class="w-25" type="number" name="amount" value="0"></p>
+            </li>
+        `;
+    });
 }
 
-async function getResponce1() {
-    let responce = await fetch("shop.json");
-    let content = await responce.text();
-    content = JSON.parse(content);
-    content = content.splice(0, 11);
-    let key;
-    content_title = content.sort((a, b) => {
-        const nameA = a.title.toUpperCase();
-        const nameB = b.title.toUpperCase();
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
-    });
-    content_filter = [];
-    let word = document.getElementById('search').value.toLowerCase();
-    content_filter = content_title.filter((product) => {
-        return (
-            product.title.toLowerCase().includes(word) ||
-            product.description.toLowerCase().includes(word) ||
-            product.price.toString().includes(word)
-        );
-    });
-    let node_for_insert = document.getElementById("node_for_insert");
-    for (key in content_filter) {
-        node_for_insert.innerHTML += `
-        <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
-        <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
-        <h5 class="card-title">${content_filter[key].title}</h5>
-        <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
-        <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
-        <p class="card-text" >Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
-        </li>
-                `;
-    }
-}
+document.getElementById("searchInput").addEventListener("input", function() {
+    let searchValue = this.value.toLowerCase();
+    let filteredContent = content.filter(item => 
+        item.title.toLowerCase().includes(searchValue)
+    );
+    renderProducts(filteredContent);
+});
 
-sort();
+getResponse();
